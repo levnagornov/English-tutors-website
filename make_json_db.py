24f699a895e942686984454fbfd4cdfe24f3222c
Lev Nagornov <lnagornov@yahoo.com>
2021-06-15 13:30:40 +0300

    first commit

---
+++ make_json_db.py
@@ -0,0 +1,8 @@
+def copy_data_to_json(data_file, json_file):
+    ''' Due to course requirements 
+    1. I must use json file as a database
+    2. I must copy info from data.py to json once 
+    '''
+    pass
+
+#copy_data_to_json('data.py', 'db.json'
